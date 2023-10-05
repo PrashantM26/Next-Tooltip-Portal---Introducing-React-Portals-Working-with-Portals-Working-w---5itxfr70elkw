7eb59a11edbece4e33578f301185d58a80665949
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-05 23:22:28 +0530

    Update TooltipPortal.js

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 
 const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
 
 function TooltipPortal({ children }) {
+  if (!portalRoot) {
+    return null;
+  }
 
+  return ReactDOM.createPortal(children, portalRoot);
 }
 
 export default TooltipPortal;
