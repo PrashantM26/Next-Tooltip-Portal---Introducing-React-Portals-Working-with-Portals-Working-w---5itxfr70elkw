c3f63850c3c90554fb09d369c86d3e047ab2a1eb
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-12 20:00:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,7 @@ import React, { useState } from 'react';
 import DynamicTooltip from '../components/DynamicTooltip';
 
 export default function Home() {
-  /*const [showTooltip, setShowTooltip] = useState(false);
-
-  const handleMouseOver = () => {
-    setShowTooltip(true);
-  };
-
-  const handleMouseOut = () => {
-    setShowTooltip(false);
-  };
-
-  return (
-    <div>
-     <span 
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-     >
-          Hover over this text to see the dynamic tooltip.
-      </span>
-      {showTooltip && <DynamicTooltip />}
-    </div>
-  );*/
+  
 
 const [showTooltip, setShowTooltip] = useState(false);
 ​
