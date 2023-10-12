6c1317bbbf4fcfffdd9d4f6ff4e6b133a3e6f180
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-12 21:16:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,7 @@ import React, { useState } from 'react';
 import DynamicTooltip from '../components/DynamicTooltip';
 
 export default function Home() {
-  
-
-const [showTooltip, setShowTooltip] = useState(false);
-​
-  
+  const [showTooltip, setShowTooltip] = useState(false);  
   return (
     <div>
      <span onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}>Hover over this text to see the dynamic tooltip.</span>
